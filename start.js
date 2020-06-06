5fc6b9299a65f4773f04cadc2906cbb0a05268bc
Giovani  Jose <giovanijose@Mac-mini-de-Giovani.local>
2020-06-05 21:11:49 -0300

    feat: adds initial config

--- start.js
+++ start.js
@@ -2,12 +2,15 @@ var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
+let users = [];
+let rooms = [];
+
 app.get("/", (req, res) => {
   res.send({ ok: true }).status(200);
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log(socket);
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
